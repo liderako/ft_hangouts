b8cf8da8b2156a8e9ebcbc207850a77d9dd33e84
asvirido <trotillostudio@gmail.com>
2018-05-07 16:42:05 +0300

    update

--- app/src/main/java/asvirido/student/com/ft_hangouts/DescriptionActivity.java
+++ app/src/main/java/asvirido/student/com/ft_hangouts/DescriptionActivity.java
@@ -9,13 +9,23 @@ import android.widget.TextView;
 public class DescriptionActivity extends AppCompatActivity {
     private Intent intent;
     private TextView nameView;
+    private TextView phoneNumberView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         intent = getIntent();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
+
         nameView = findViewById(R.id.nameView);
+        phoneNumberView = findViewById(R.id.phoneNumberView);
+
         String name = intent.getStringExtra("Name");
+        String phoneNumber = intent.getStringExtra("PhoneNumber");
+        Log.d("CREATE", name);
+        Log.d("CREATE", phoneNumber);
         nameView.setText(name);
+        phoneNumberView.setText(phoneNumber);
     }
 }
--- app/src/main/java/asvirido/student/com/ft_hangouts/MainActivity.java
+++ app/src/main/java/asvirido/student/com/ft_hangouts/MainActivity.java
@@ -15,6 +15,7 @@ import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,21 +34,21 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView contactList;
 
-    private ArrayList<String> contacts = new ArrayList<String>();
+    private ArrayList<String> contactsNameList = new ArrayList<String>();
+    private ArrayList<String> contactsPhoneList = new ArrayList<String>();
     private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         contactList = findViewById(R.id.contactList);
-        // получаем разрешения
+        /* получаем разрешения */
         int hasReadContactPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
-        // если устройство до API 23, устанавливаем разрешение
+        /* если устройство до API 23, устанавливаем разрешение */
         if (hasReadContactPermission == PackageManager.PERMISSION_GRANTED){
             READ_CONTACTS_GRANTED = true;
         } else {
@@ -74,33 +75,58 @@ public class MainActivity extends AppCompatActivity {
         return (id == R.id.action_settings || super.onOptionsItemSelected(item));
     }
 
-    private void loadContacts(){
+    private void loadContacts() {
+        loadNameContact();
+        loadPhoneNumberContact();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactsNameList);
+        contactList.setAdapter(adapter);
+        contactList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    intent = new Intent(MainActivity.this, DescriptionActivity.class);
+                    intent.putExtra("Name", contactsNameList.get(position));
+                    intent.putExtra("PhoneNumber", contactsPhoneList.get(position));
+                    startActivity(intent);
+                    return false;
+            }
+        });
+    }
+
+    private void loadNameContact() {
         ContentResolver contentResolver = getContentResolver();
         Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
         if (cursor != null) {
             while (cursor.moveToNext()) {
                 String contact = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
-                contacts.add(contact);
+                contactsNameList.add(contact);
             }
             cursor.close();
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts);
-        contactList.setAdapter(adapter);
+    }
 
-        contactList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    private void loadPhoneNumberContact() {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null,null, null, null);
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
 
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                intent = new Intent(MainActivity.this, DescriptionActivity.class);
-                intent.putExtra("Name", contacts.get(position));
-                startActivity(intent);
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+                    while (phones.moveToNext()) {
+                        String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        contactsPhoneList.add(phoneNumber);
+                    }
+                    phones.close();
+                }
             }
-        });
+            cur.close();
+        }
     }
 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-
         switch (requestCode) {
             case REQUEST_CODE_READ_CONTACTS:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
